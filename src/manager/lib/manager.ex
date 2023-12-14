1944c17386245c666e1773d396f5f24cafc8c0e8
ArmandoCivini <acivini@fi.uba.ar>
2023-12-14 16:07:55 -0300

    added split evenly function

--- src/manager/lib/manager.ex
+++ src/manager/lib/manager.ex
@@ -19,5 +19,15 @@ defmodule Manager do
     interval = Interval.newInterval(0, 10, 1)
     IO.inspect(interval)
     IO.puts Interval.round_float(1.23456789, 2)
+    IO.inspect(Interval.split_evenly(interval, 2))
+    # value1 = Decimal.new("5.0000000000")
+    # value2 = Decimal.new("0.0000000001")
+
+    # IO.inspect value1
+    # IO.inspect value2
+
+    # result = value1 - value2
+
+    # IO.puts "Result: #{Decimal.to_string(result)}"
   end
 end
