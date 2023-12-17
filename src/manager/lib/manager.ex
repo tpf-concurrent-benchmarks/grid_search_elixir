b6990e65ff6b668a7afe47b7dbcbff0d91644978
Facundo Mastricchio <facundomastricchio@gmail.com>
2023-12-17 18:58:09 -0300

    Remove Manager docs

--- src/manager/lib/manager.ex
+++ src/manager/lib/manager.ex
@@ -1,18 +1,4 @@
 defmodule Manager do
-  @moduledoc """
-  Documentation for `Manager`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Manager.hello()
-      :world
-
-  """
-
 
   def main do
     IO.puts "Hello, world!"
