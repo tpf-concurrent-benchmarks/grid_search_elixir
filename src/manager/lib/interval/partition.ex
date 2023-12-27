0d521b45026fe38b8ecec9cb237ac7cc648c73a3
ArmandoCivini <acivini@fi.uba.ar>
2023-12-27 20:41:40 -0300

    added fixed elements cartesian product for partitions

--- src/manager/lib/interval/partition.ex
+++ src/manager/lib/interval/partition.ex
@@ -99,4 +99,9 @@ defmodule Partition do
       partitionGenerator(tail, senderFun, [elem])
     end
   end
+
+  #this bounded version may be faster than the recursive one
+  def partitionGenerator3elems(x, y, z, senderFun) do
+    for elemX <- x, elemY <- y, elemZ <- z do: senderFun.(elemX, elemY, elemZ)
+  end
 end
