4616124a5f11ad4457656e56e5a383ca946cc787
ArmandoCivini <acivini@fi.uba.ar>
2024-01-03 16:40:27 -0300

    added more partition tests

--- src/manager/test/manager_test.exs
+++ src/manager/test/manager_test.exs
@@ -2,7 +2,7 @@ defmodule ManagerTest do
   use ExUnit.Case
   doctest Manager
 
-  test "TestPartitionsOne" do
+  test "TestNewPartition" do
     intervals = [
       Interval.newInterval(0, 10, 5),
       Interval.newInterval(0, 10, 5),
@@ -34,5 +34,83 @@ defmodule ManagerTest do
 
     expected = [3, 1, 1]
     assert partitionPerInterval == expected
+  end
+
+  # this tests are not perfect, they can create false positives
+  # because of the flattening of the list but it is highly unlikely
+  test "TestPartitionsOne" do
+    intervals = [
+      Interval.newInterval(0, 10, 5),
+      Interval.newInterval(0, 10, 5),
+      Interval.newInterval(0, 10, 5)
+    ]
+
+    nIntervals = length(intervals)
+    maxChunkSize = 5
+    partition = Partition.newPartition(intervals, nIntervals, maxChunkSize)
+
+    expected = [
+      [
+        Interval.newInterval(0, 5, 5),
+        Interval.newInterval(0, 10, 5),
+        Interval.newInterval(0, 10, 5)
+      ],
+      [
+        Interval.newInterval(5, 10, 5),
+        Interval.newInterval(0, 10, 5),
+        Interval.newInterval(0, 10, 5)
+      ]
+    ]
+    expected = List.flatten(expected) |> Enum.sort
+
+    mock_callback = fn element -> element end
+
+    actual = Partition.partitionGenerator(partition.splitIntervals, mock_callback, [])
+    actual = List.flatten(actual) |> Enum.sort
+
+    assert actual == expected
+  end
+
+  test "TestPartitionsMultiple" do
+    intervals = [
+      Interval.newInterval(0, 10, 5),
+      Interval.newInterval(0, 10, 5),
+      Interval.newInterval(0, 10, 5)
+    ]
+
+    nIntervals = length(intervals)
+    maxChunkSize = 4
+    partition = Partition.newPartition(intervals, nIntervals, maxChunkSize)
+
+    expected = [
+      [
+        Interval.newInterval(0, 5, 5),
+        Interval.newInterval(0, 5, 5),
+        Interval.newInterval(0, 10, 5)
+      ],
+      [
+        Interval.newInterval(5, 10, 5),
+        Interval.newInterval(0, 5, 5),
+        Interval.newInterval(0, 10, 5)
+      ],
+      [
+        Interval.newInterval(0, 5, 5),
+        Interval.newInterval(5, 10, 5),
+        Interval.newInterval(0, 10, 5)
+      ],
+      [
+        Interval.newInterval(5, 10, 5),
+        Interval.newInterval(5, 10, 5),
+        Interval.newInterval(0, 10, 5)
+      ]
+    ]
+    expected = List.flatten(expected) |> Enum.sort
+
+    mock_callback = fn element -> element end
+
+    actual = Partition.partitionGenerator(partition.splitIntervals, mock_callback, [])
+    actual = List.flatten(actual) |> Enum.sort
+
+    assert actual == expected
   end
 end
