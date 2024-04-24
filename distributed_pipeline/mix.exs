d03f4526ea39058144bf2e2c3ac3c33120d46aff
efoppiano <efoppiano@fi.uba.ar>
2024-04-23 21:48:33 -0300

    Remove unused library

--- distributed_pipeline/mix.exs
+++ distributed_pipeline/mix.exs
@@ -23,8 +23,7 @@ defmodule DistributedPipeline.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:jason, "~> 1.4"},
-      {:image, "~> 0.37"}
+      {:jason, "~> 1.4"}
     ]
   end
 end
